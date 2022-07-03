ee29b7a7d3f37ab72b41a3cd6ffbdfee9fafad23
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-03 08:29:38 -0400

    updated formatting

--- App.js
+++ App.js
@@ -63,7 +63,7 @@ class ReactApp extends React.Component {
             <a id="tweet-quote" src target="_blank" href="https://www.twitter.com/intent/tweet"><i class="fa fa-twitter"></i></a>
           </div>
         </div>
-          <div className="footer">
+        <div className="footer">
           <div id="name">Jeremy Duncan &copy; 2022</div>
           <div id="page"><a href="https://github.com/JeremyDuncan">Jeremy's GitHub</a> </div>
         </div>
